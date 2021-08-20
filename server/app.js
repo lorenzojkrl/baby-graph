1eac1cbabf81a99cda123f24246240b55bf819e9
Lorenzo <lorenzozar@gmail.com>
2021-08-20 16:26:58 +0300

    add graphiql middleware

--- server/app.js
+++ server/app.js
@@ -1,8 +1,21 @@
 const express = require('express');
+const graphqlHTTP = require('express-graphql');
 
 const app = express();
 
-// run with node app 
+app.use('/mygraphql', graphqlHTTP({
+  graphiql: true // when we serve mygraphql, graphiql should be shown
+}))
+
+// run with node app to see the app at localhost:4000
 app.listen(4000, () => {
   console.log('listening for requests');
 })
+
+
+  ```
+npm init
+npm install express --save
+npm install graphql express-graphql
+npm install nodemon
+```
