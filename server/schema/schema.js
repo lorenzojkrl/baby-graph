963f372f516b30f865e5665582ddc61f40ffd3dd
Lorenzo <lorenzozar@gmail.com>
2021-08-20 17:45:19 +0300

    Create RootQuery, export schema

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -5,7 +5,8 @@ const graphql = require('graphql')
 //   GraphQLObjectType,
 //   GraphQLID,
 //   GraphQLString,
-//   GraphQLInt
+//   GraphQLInt,
+//   GraphQLSchema
 // } = graphql
 
 // Create types
@@ -15,8 +16,29 @@ const UserType = new graphql.GraphQLObjectType({
   // fields contains actual data of User
   // callback f return an object that specifies the types of props
   fields: () => ({
-    id: { type: graphql.GraphQLID },
+    id: { type: graphql.GraphQLString },
     name: { type: graphql.GraphQLString },
     age: { type: graphql.GraphQLInt }
   })
+})
+
+// RootQuery
+// RootQuery is the "path" that allows to reach nodes 
+const RootQuery = new graphql.GraphQLObjectType({
+  name: "RootQueryType", // for documentation purposes
+  description: "",
+  fields: { // this is going to be the query structure
+    user: {
+      type: UserType,
+      args: { id: { type: graphql.GraphQLString } }, // we may want to pass some args e.g. id to retrieve a specific user
+      resolve(parent, args) { // resolve with data. In this case, parents refer to UserType and args to the args obj
+        // get & return data from a data source
+      }
+    }
+  }
+})
+
+// export schema's query
+module.exports = new graphql.GraphQLSchema({
+  query: RootQuery
 })
